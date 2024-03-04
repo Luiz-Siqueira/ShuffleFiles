f6c00ab6abbccbde3a1579545dcbc2f19079b774
Luiz Siqueira <luiz.gomees400@gmail.com>
2024-03-04 14:39:12 -0300

    feat: adiciona interface para escolher o diretorio dos arquivos que serão adicionados

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import random
+from tkinter import *
+from tkinter import filedialog
 
-folder = r'E:\\'
+global folder
 
 import re
 
@@ -12,16 +14,45 @@ def remove_number_hyphen(value):
     
     return val_whithout_number_with_hyphen
 
-for file_name in os.listdir(folder):
-    old_name = folder + file_name
+def alter_name_file():
+    for file_name in os.listdir(folder):
+        old_name = folder + file_name
 
-    if ' - ' in file_name:
-        file_name = remove_number_hyphen(file_name)
+        if ' - ' in file_name:
+            file_name = remove_number_hyphen(file_name)
 
-    indice_order = random.randint(0, 2**31 - 1)
-    new_name = folder + str(indice_order) + ' - ' + file_name
+        indice_order = random.randint(0, 2**31 - 1)
+        new_name = folder + str(indice_order) + ' - ' + file_name
 
-    os.rename(old_name, new_name)
+        os.rename(old_name, new_name)
 
 
-print(os.listdir(folder))
+def open_dir():
+    diretorio = filedialog.askdirectory()
+    if diretorio:
+        input_entry.config(state='normal')
+        folder = 'r'+diretorio
+        input_entry.insert(0, diretorio)
+
+        print("Diretório selecionado:", diretorio)
+        input_entry.config(state='readonly')
+
+
+janela = Tk()
+janela.title("ShuffleFiles")
+
+text_orientacao = Label(janela, text="Escolha um diretorio onde os arquivos se encontram").grid(column=0, row=0)
+frame = Frame(janela)
+frame.grid(row=1, column=0, padx=10, pady=10)
+
+
+input_entry = Entry(frame, font=30)
+input_entry.grid(row=0, column=0, padx=0)
+input_entry.config(state='readonly')
+
+botao = Button(frame, text="Buscar diretorio", command=open_dir).grid(row=0, column=1, padx=0)
+
+
+janela.mainloop()
+
+
